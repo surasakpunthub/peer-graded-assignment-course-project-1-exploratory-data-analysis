040033633afebdc5899e09e366dfda056ace13dc
surasakpunthub <49191591+surasakpunthub@users.noreply.github.com>
2019-08-06 13:24:00 +0700

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,16 @@
+# set dataset directory
+setwd("/Users/surasakpunthub/Desktop/Digital triz/R programing/Couse-4/W-1/Assignment")
+
+
+
+dataFile <- "./household_power_consumption.txt"
+
+
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
